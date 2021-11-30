118e7b58e58a5beef230680f70dc7be348a2e68c
Eric Zhu <73148494+ericzhu98@users.noreply.github.com>
2021-12-01 04:28:17 +1100

    Demo front page

--- src/views/Agency/Agency.js
+++ src/views/Agency/Agency.js
@@ -18,6 +18,7 @@ import {
   Contact,
 } from './components';
 
+
 const Agency = () => {
   useEffect(() => {
     const jarallaxInit = async () => {
@@ -63,42 +64,44 @@ const Agency = () => {
     filter: theme.palette.mode === 'dark' ? 'brightness(0.7)' : 'none',
   });
 
-  return (
+
+
+    return (
     <Main>
-      <Box
-        minHeight={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        bgcolor={'alternate.main'}
-        marginTop={-13}
-        paddingTop={13}
-      >
-        <Container>
-          <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
-            <Welcome />
-            <Box marginTop={4}>
-              <NoSsr>
-                <Box
-                  component={'svg'}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  width={{ xs: 30, sm: 40 }}
-                  height={{ xs: 30, sm: 40 }}
-                  onClick={() => scrollTo('agency__portfolio-item--js-scroll')}
-                  sx={{ cursor: 'pointer' }}
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </Box>
-              </NoSsr>
-            </Box>
-          </Box>
-        </Container>
-      </Box>
+      {/*<Box*/}
+      {/*  minHeight={'100vh'}*/}
+      {/*  display={'flex'}*/}
+      {/*  alignItems={'center'}*/}
+      {/*  bgcolor={'alternate.main'}*/}
+      {/*  marginTop={-13}*/}
+      {/*  paddingTop={13}*/}
+      {/*>*/}
+      {/*  <Container>*/}
+      {/*    <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>*/}
+      {/*      <Welcome />*/}
+      {/*      <Box marginTop={4}>*/}
+      {/*        <NoSsr>*/}
+      {/*          <Box*/}
+      {/*            component={'svg'}*/}
+      {/*            xmlns="http://www.w3.org/2000/svg"*/}
+      {/*            viewBox="0 0 20 20"*/}
+      {/*            fill="currentColor"*/}
+      {/*            width={{ xs: 30, sm: 40 }}*/}
+      {/*            height={{ xs: 30, sm: 40 }}*/}
+      {/*            onClick={() => scrollTo('agency__portfolio-item--js-scroll')}*/}
+      {/*            sx={{ cursor: 'pointer' }}*/}
+      {/*          >*/}
+      {/*            <path*/}
+      {/*              fillRule="evenodd"*/}
+      {/*              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"*/}
+      {/*              clipRule="evenodd"*/}
+      {/*            />*/}
+      {/*          </Box>*/}
+      {/*        </NoSsr>*/}
+      {/*      </Box>*/}
+      {/*    </Box>*/}
+      {/*  </Container>*/}
+      {/*</Box>*/}
       <Box
         className={'jarallax'}
         data-jarallax
@@ -112,7 +115,7 @@ const Agency = () => {
         <Box
           className={'jarallax-img'}
           sx={styles(
-            'https://assets.maccarianagency.com/backgrounds/img48.jpg',
+            'https://raw.githubusercontent.com/ericfzhu/demo/master/src/images/blue%20banner.png',
           )}
         />
         <Container>
@@ -131,23 +134,23 @@ const Agency = () => {
         <Box
           className={'jarallax-img'}
           sx={styles(
-            'https://assets.maccarianagency.com/backgrounds/img44.jpg',
+            'https://raw.githubusercontent.com/ericfzhu/demo/master/src/images/pink%20banner.png',
           )}
         />
         <Container>
           <Larq />
         </Container>
       </Box>
-      <Box
-        minHeight={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        bgcolor={'alternate.main'}
-      >
-        <Container>
-          <Process />
-        </Container>
-      </Box>
+      {/*<Box*/}
+      {/*  minHeight={'100vh'}*/}
+      {/*  display={'flex'}*/}
+      {/*  alignItems={'center'}*/}
+      {/*  bgcolor={'alternate.main'}*/}
+      {/*>*/}
+      {/*  <Container>*/}
+      {/*    <Process />*/}
+      {/*  </Container>*/}
+      {/*</Box>*/}
       <Box
         className={'jarallax'}
         data-jarallax
@@ -156,93 +159,94 @@ const Agency = () => {
         minHeight={'100vh'}
         display={'flex'}
         alignItems={'center'}
-        id="agency__portfolio-item--js-scroll"
+        // id="agency__portfolio-item--js-scroll"
       >
         <Box
           className={'jarallax-img'}
           sx={styles(
-            'https://assets.maccarianagency.com/backgrounds/img45.jpg',
+            'https://raw.githubusercontent.com/ericfzhu/demo/master/src/images/purple%20banner.png',
           )}
         />
         <Container>
           <Nike />
         </Container>
       </Box>
-      <Box
-        className={'jarallax'}
-        data-jarallax
-        data-speed="0.2"
-        position={'relative'}
-        minHeight={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        id="agency__portfolio-item--js-scroll"
-      >
-        <Box
-          className={'jarallax-img'}
-          sx={styles(
-            'https://assets.maccarianagency.com/backgrounds/img43.jpg',
-          )}
-        />
-        <Container>
-          <Trek />
-        </Container>
-      </Box>
+      {/*<Box*/}
+      {/*  className={'jarallax'}*/}
+      {/*  data-jarallax*/}
+      {/*  data-speed="0.2"*/}
+      {/*  position={'relative'}*/}
+      {/*  minHeight={'100vh'}*/}
+      {/*  display={'flex'}*/}
+      {/*  alignItems={'center'}*/}
+      {/*  id="agency__portfolio-item--js-scroll"*/}
+      {/*>*/}
+      {/*  <Box*/}
+      {/*    className={'jarallax-img'}*/}
+      {/*    sx={styles(*/}
+      {/*      'https://assets.maccarianagency.com/backgrounds/img43.jpg',*/}
+      {/*    )}*/}
+      {/*  />*/}
+      {/*  <Container>*/}
+      {/*    <Trek />*/}
+      {/*  </Container>*/}
+      {/*</Box>*/}
+      {/*<Box*/}
+      {/*  minHeight={'100vh'}*/}
+      {/*  display={'flex'}*/}
+      {/*  alignItems={'center'}*/}
+      {/*  bgcolor={'alternate.main'}*/}
+      {/*>*/}
+      {/*  <Container>*/}
+      {/*    <Reviews />*/}
+      {/*  </Container>*/}
+      {/*</Box>*/}
+      {/*<Box*/}
+      {/*  className={'jarallax'}*/}
+      {/*  data-jarallax*/}
+      {/*  data-speed="0.2"*/}
+      {/*  position={'relative'}*/}
+      {/*  minHeight={'100vh'}*/}
+      {/*  display={'flex'}*/}
+      {/*  alignItems={'center'}*/}
+      {/*  id="agency__portfolio-item--js-scroll"*/}
+      {/*>*/}
+      {/*  <Box*/}
+      {/*    className={'jarallax-img'}*/}
+      {/*    sx={styles(*/}
+      {/*      'https://assets.maccarianagency.com/backgrounds/img47.jpg',*/}
+      {/*    )}*/}
+      {/*  />*/}
+      {/*  <Container>*/}
+      {/*    <Curology />*/}
+      {/*  </Container>*/}
+      {/*</Box>*/}
+      {/*<Box*/}
+      {/*  className={'jarallax'}*/}
+      {/*  data-jarallax*/}
+      {/*  data-speed="0.2"*/}
+      {/*  position={'relative'}*/}
+      {/*  minHeight={'100vh'}*/}
+      {/*  display={'flex'}*/}
+      {/*  alignItems={'center'}*/}
+      {/*  id="agency__portfolio-item--js-scroll"*/}
+      {/*>*/}
+      {/*  <Box*/}
+      {/*    className={'jarallax-img'}*/}
+      {/*    sx={styles(*/}
+      {/*      'https://assets.maccarianagency.com/backgrounds/img46.jpg',*/}
+      {/*    )}*/}
+      {/*  />*/}
+      {/*  <Container>*/}
+      {/*    <Nike2 />*/}
+      {/*  </Container>*/}
+      {/*</Box>*/}
       <Box
         minHeight={'100vh'}
         display={'flex'}
         alignItems={'center'}
         bgcolor={'alternate.main'}
-      >
-        <Container>
-          <Reviews />
-        </Container>
-      </Box>
-      <Box
-        className={'jarallax'}
-        data-jarallax
-        data-speed="0.2"
-        position={'relative'}
-        minHeight={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        id="agency__portfolio-item--js-scroll"
-      >
-        <Box
-          className={'jarallax-img'}
-          sx={styles(
-            'https://assets.maccarianagency.com/backgrounds/img47.jpg',
-          )}
-        />
-        <Container>
-          <Curology />
-        </Container>
-      </Box>
-      <Box
-        className={'jarallax'}
-        data-jarallax
-        data-speed="0.2"
-        position={'relative'}
-        minHeight={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        id="agency__portfolio-item--js-scroll"
-      >
-        <Box
-          className={'jarallax-img'}
-          sx={styles(
-            'https://assets.maccarianagency.com/backgrounds/img46.jpg',
-          )}
-        />
-        <Container>
-          <Nike2 />
-        </Container>
-      </Box>
-      <Box
-        minHeight={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        bgcolor={'alternate.main'}
+        id={"contact-us"}
       >
         <Container>
           <Contact />
