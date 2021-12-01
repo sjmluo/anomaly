c908be69dd70689e3c1557f279eb92a17d64cb9d
Eric Zhu <73148494+ericzhu98@users.noreply.github.com>
2021-12-01 18:23:34 +1100

    Added navigation

---
+++ src/layouts/navigation.js
@@ -0,0 +1,20 @@
+const pages = {
+  aboutUs: [
+    {
+      title: 'Our Mission',
+      href: '/mission',
+    },
+    {
+      title: 'Meet the team',
+      href: '/team',
+    }
+  ],
+  ourWork: [
+    {
+      title: 'Blog',
+      href: '/blog',
+    }
+  ]
+};
+
+export default pages;
