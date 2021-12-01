d2ff7751b736d483dd0ebf231790c906e4659455
Eric Zhu <73148494+ericzhu98@users.noreply.github.com>
2021-12-01 22:42:02 +1100

    Added index

---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+export { default as IndexView } from './IndexView';
+export { default as About } from './About';
+export { default as Team } from './Team';
+export { default as Blog } from './Blog';
+export { default as NotFound } from './NotFound';
