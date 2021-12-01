432c938c80f64956bf69364ecffbe6e85824415e
Eric Zhu <73148494+ericzhu98@users.noreply.github.com>
2021-12-01 23:02:01 +1100

    Edited config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,44 +4,9 @@ module.exports = {
     title: "demo",
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "eGkiZ0uCFSWiIuhf02hCWIuQdtaTp7xjFnFrI_vcStI",
-        spaceId: "j1i7j7n6v3mr",
-      },
-    },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     'gatsby-plugin-mui-emotion',
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-top-layout',
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/logo.svg",
-      },
-    },
-    "gatsby-transformer-remark",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
   ],
 };
