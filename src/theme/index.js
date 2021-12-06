41b6a67ca1378094d2236cd4561eacef5086880f
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-07 09:05:41 +1100

    Updated to Gatsby v4

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,7 +10,7 @@ const getTheme = (mode, themeToggler) =>
       palette: mode === 'light' ? light : dark,
       shadows: shadows(mode),
       typography: {
-        fontFamily: '"Montserrat", sans-serif',
+        fontFamily: "Montserrat",
         button: {
           textTransform: 'none',
           fontWeight: 'medium',
