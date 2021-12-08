41a04ae6165eb0722b36d03793e3faf058a5087c
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-08 16:44:04 +1100

    Initial GraphQL query design for all blog post nodes

--- src/views/Blog/components/Articles/Articles.js
+++ src/views/Blog/components/Articles/Articles.js
@@ -3,29 +3,132 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { useTheme } from '@mui/material/styles';
 import { graphql } from 'gatsby';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { Button } from 'gatsby-theme-material-ui';
+import { renderRichText } from 'gatsby-source-contentful/rich-text';
 
 export const pageQuery = graphql`
-  query MyQuery {
-    allContentfulBlogPost {
-      nodes {
-        body {
-          raw
-        }
-        contentful_id
-        date
-        url
-        author {
-          name
+    query {
+      allContentfulBlogPost {
+        nodes {
+          contentful_id
+          date
+          createdAt
+          author {
+            name
+          }
+          body {
+            raw
+          }
+          title
+          image {
+            gatsbyImageData(layout: FIXED, width: 75)
+          }
+          gatsbyPath(filePath: "/blog/{contentfulBlogPost.title}")
         }
       }
     }
-  }
 `
+
+const Blog = ({ node }) => (
+    <Grid item xs={12} key={i}>
+      <Box
+          component={Card}
+          width={1}
+          height={1}
+          borderRadius={0}
+          boxShadow={0}
+          display={'flex'}
+          flexDirection={{ xs: 'column', md: 'row' }}
+          sx={{ backgroundImage: 'none', bgcolor: 'transparent' }}
+      >
+        <Box
+            sx={{
+              width: { xs: 1, md: '30%' },
+              '& .lazy-load-image-loaded': {
+                height: 1,
+                display: 'flex !important',
+              },
+            }}
+        >
+            {node.image[0].gatsbyImageData && (
+                <Box
+                    component={GatsbyImage}
+                    height={1}
+                    width={1}
+                    image={node.image[0].gatsbyImageData}
+                    effect="blur"
+                    sx={{
+                        objectFit: 'cover',
+                        maxHeight: 200,
+                        borderRadius: 2,
+                        filter:
+                            theme.palette.mode === 'dark'
+                                ? 'brightness(0.7)'
+                                : 'none',
+                    }}
+                />
+            )}
+        </Box>
+        <CardContent
+            sx={{
+              width: { xs: 1, md: '70%' },
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+        >
+          <Typography
+              fontWeight={700}
+              sx={{ textTransform: 'uppercase' }}
+          >
+            {node.title}
+          </Typography>
+          <Box marginY={1}>
+            <Typography
+                variant={'caption'}
+                color={'text.secondary'}
+                component={'i'}
+            >
+              {node.author.name} - {node.date}
+            </Typography>
+          </Box>
+          <Typography color="text.secondary">
+            {node.body}
+          </Typography>
+          <Box marginTop={2} display={'flex'} justifyContent={'flex-end'}>
+            <Button
+                to={node.gatsbyPath}
+                endIcon={
+                  <Box
+                      component={'svg'}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      width={24}
+                      height={24}
+                  >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                  </Box>
+                }
+            >
+              Read More
+            </Button>
+          </Box>
+        </CardContent>
+      </Box>
+    </Grid>
+)
 
 const mock = [
   {
@@ -68,6 +171,7 @@ const mock = [
 
 const Articles = () => {
   const theme = useTheme();
+  const blogNodes = this.props.data;
   return (
     <Box>
       <Grid container spacing={4}>
