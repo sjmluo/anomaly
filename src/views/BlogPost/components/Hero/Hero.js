d70a0813ca6b4af44fe5a5fea66c6a4086d193b0
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-09 17:42:31 +1100

    Moved individual blog posts into separate view page

---
+++ src/views/BlogPost/components/Hero/Hero.js
@@ -0,0 +1,101 @@
+import React, { useEffect } from 'react';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+
+import Container from 'components/Container';
+
+const Hero = ({ data }) => {
+  useEffect(() => {
+    const jarallaxInit = async () => {
+      const jarallaxElems = document.querySelectorAll('.jarallax');
+      if (!jarallaxElems || (jarallaxElems && jarallaxElems.length === 0)) {
+        return;
+      }
+
+      const { jarallax } = await import('jarallax');
+      jarallax(jarallaxElems, { speed: 0.2 });
+    };
+
+    jarallaxInit();
+  });
+
+  return (
+    <Box
+      className={'jarallax'}
+      data-jarallax
+      data-speed="0.2"
+      position={'relative'}
+      minHeight={{ xs: 400, sm: 500, md: 600 }}
+      display={'flex'}
+      marginTop={-13}
+      paddingTop={13}
+      alignItems={'center'}
+      id="agency__portfolio-item--js-scroll"
+    >
+      <Box
+        className={'jarallax-img'}
+        sx={{
+          position: 'absolute',
+          objectFit: 'cover',
+          fontFamily: 'object-fit: cover;',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
+          // backgroundImage:
+          //   'url(https://assets.maccarianagency.com/backgrounds/img3.jpg)',
+        }}
+      />
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          width: 1,
+          height: 1,
+          background: alpha('#161c2d', 0.6),
+          zIndex: 1,
+        }}
+      />
+      <Container position={'relative'} zIndex={2}>
+        <Box>
+          <Typography
+            variant="h3"
+            sx={{
+              fontWeight: 400,
+              color: 'common.white',
+              marginBottom: 2,
+            }}
+          >
+              {data.title}
+          </Typography>
+          <Box display={'flex'} alignItems={'center'}>
+            <ListItemText
+              sx={{ margin: 0 }}
+              primary={data.author.name}
+              secondary={data.createdAt}
+              primaryTypographyProps={{
+                variant: 'h6',
+                sx: { color: 'common.white' },
+              }}
+              secondaryTypographyProps={{
+                sx: { color: alpha('#ffffff', 0.8) },
+              }}
+            />
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Hero;
