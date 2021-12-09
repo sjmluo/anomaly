80dc1f6921c5eaccc4bd62c30240cdffd9ab7afa
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-09 17:41:28 +1100

    Fixed images not appearing in contentful richtext

---
+++ src/pages/blog/{contentfulBlogPost.title}.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import BlogPost from 'views/BlogPost';
+import { graphql } from "gatsby";
+
+const BlogPostPage = ({ data }) => {
+    return <BlogPost data={data.contentfulBlogPost}/>;
+}
+
+
+export default BlogPostPage;
+
+export const query = graphql`
+    query ($id: String) {
+        contentfulBlogPost(id: {eq: $id}) {
+            author {
+                name
+            }
+            title
+            createdAt(formatString: "DD MMMM YYYY")
+            body {
+                raw
+                references {
+                    contentful_id
+                    description
+                    gatsbyImageData(width: 1000)
+                    __typename
+                }
+            }
+        }
+    }
+`
