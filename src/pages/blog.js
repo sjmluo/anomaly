42d16b0c160f0dc807e3c4170b3cde643ae50c45
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-09 11:25:00 +1100

    Removed unused graphql query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import Blog from 'views/Blog';
-import {graphql} from "gatsby";
 
 const BlogPage = () => {
   return <Blog />;
 };
-
-export const query = graphql`
-    query {
-      allContentfulBlogPost {
-        nodes {
-          contentful_id
-          date
-          createdAt
-          author {
-            name
-          }
-          body {
-            raw
-          }
-          title
-          image {
-            gatsbyImageData(layout: FIXED, width: 75)
-          }
-          gatsbyPath(filePath: "/blog/{contentfulBlogPost.title}")
-        }
-      }
-    }
-`
 
 export default BlogPage;
