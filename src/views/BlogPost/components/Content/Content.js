0e83cee3f0cce4aaf73fe209a55eddd9b35ab31f
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-09 18:07:30 +1100

    Added additional transform options for richtext formatting

--- src/views/BlogPost/components/Content/Content.js
+++ src/views/BlogPost/components/Content/Content.js
@@ -19,16 +19,79 @@ const Content = ({ data }) => {
   });
     const options = {
         renderMark: {
-            [MARKS.BOLD]: (text) => <b className="font-bold"><font color={theme.palette.primary.main}>{text}</font></b>
+            [MARKS.BOLD]: (text) => <b><font color={theme.palette.primary.main}>{text}</font></b>,
+            [MARKS.ITALIC]: (text) => <i><font color={theme.palette.primary.main}>{text}</font></i>,
+            [MARKS.UNDERLINE]: (text) => <u>{text}</u>
         },
         renderNode: {
+            [INLINES.HYPERLINK]: (node, children) => (
+                <a
+                    href={node.data.uri}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    {children}
+                </a>
+            ),
+            [BLOCKS.HEADING_1]: (node, children) => (
+                <h1>
+                    {children}
+                </h1>
+            ),
+            [BLOCKS.HEADING_2]: (node, children) => (
+                <h2>
+                    {children}
+                </h2>
+            ),
+            [BLOCKS.HEADING_3]: (node, children) => (
+                <h3>
+                    {children}
+                </h3>
+            ),
+            [BLOCKS.HEADING_4]: (node, children) => (
+                <h4>
+                    {children}
+                </h4>
+            ),
+            [BLOCKS.HEADING_5]: (node, children) => (
+                <h5>
+                    {children}
+                </h5>
+            ),
+            [BLOCKS.HEADING_6]: (node, children) => (
+                <h6>
+                    {children}
+                </h6>
+            ),
+
+            [BLOCKS.OL_LIST]: (node, children) => (
+                <ol>{children}</ol>
+            ),
+            [BLOCKS.UL_LIST]: (node, children) => (
+                <ul>{children}</ul>
+            ),
+
+            [BLOCKS.LIST_ITEM]: (node, children) => (
+                <li>{children}</li>
+            ),
+            [BLOCKS.PARAGRAPH]: (node, children) => {
+                if (node.content[0].value === '') {
+                    return <br />
+                } else {
+                    return <p>{children}</p>
+                }
+            },
+            [BLOCKS.QUOTE]: (children) => (
+                <blockquote>
+                    <>"{children.content[0].content[0].value}"</>
+                </blockquote>
+            ),
             [BLOCKS.EMBEDDED_ASSET]: (node) => {
                 const { gatsbyImageData, description } = node.data.target
                 return (
                     <GatsbyImage
                         image={getImage(gatsbyImageData)}
                         alt={description}
-                        className="mb-10"
                     />
                 )
             },
