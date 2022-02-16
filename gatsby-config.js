1a50d515f25f361e09f7403a7b0a974b3c98316b
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-02-16 15:12:25 +1100

    Added gh pages package and deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 module.exports = {
+  pathPrefix: "/anomaly",
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "demo",
